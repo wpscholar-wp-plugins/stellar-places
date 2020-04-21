c517f4188639e970d83ab3cab5066eca656dfdb3
Micah Wood <micah@wpscholar.com>
2020-04-20 22:50:55 -0400

    Setup GitHub workflows Resolves issue #24

---
+++ .github/scripts/cleanup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+rm -rf .git
+rm -rf .github
+rm -rf .wporg
+rm -rf node_modules
+find . -name '.*' -type f -maxdepth 1 -delete
+find . -name '*.md' -type f -maxdepth 1 -delete
+find . -name '*.json' -type f -maxdepth 1 -delete
+find . -name '*.lock' -type f -maxdepth 1 -delete
+find . -name '*.xml' -type f -maxdepth 1 -delete
+find . -name '*.yml' -type f -maxdepth 1 -delete
+rm -f webpack.config.js
