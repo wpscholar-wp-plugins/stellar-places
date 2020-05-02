ef1b069a98886dbb686978130fb6864d5712ce68
Micah Wood <micah@wpscholar.com>
2020-05-02 11:04:28 -0400

    Clean up Stellar_Places_Query constructor

--- includes/queries/places-query.php
+++ includes/queries/places-query.php
@@ -11,16 +11,26 @@ class Stellar_Places_Query extends WP_Query {
 	 * @param string $query The query.
 	 */
 	public function __construct( $query = '' ) {
-		$query_args                 = wp_parse_args(
+		$query_args = wp_parse_args(
 			$query,
 			array(
 				'post_type'      => Stellar_Places::get_post_types(),
 				'posts_per_page' => - 1,
 			)
 		);
-		$query_args['meta_query'][] = array( 'key' => '_stlr_places_latitude' );
-		$query_args['meta_query'][] = array( 'key' => '_stlr_places_longitude' );
-		$query_args                 = apply_filters( 'stellar_places_query', $query_args );
+
+		$query_args['meta_query'][] = array(
+			'key'     => '_stlr_places_latitude',
+			'compare' => 'EXISTS',
+		);
+
+		$query_args['meta_query'][] = array(
+			'key'     => '_stlr_places_longitude',
+			'compare' => 'EXISTS',
+		);
+
+		$query_args = apply_filters( 'stellar_places_query', $query_args );
+
 		parent::__construct( $query_args );
 	}
 
